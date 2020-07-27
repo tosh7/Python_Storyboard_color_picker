4bb10454716dddab6b175ae237f6cdc75bc03785
tosh7 <zlia.6.lj.425@gmail.com>
2020-07-27 12:46:17 +0900

    got red color code

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ def main():
         config = yaml.safe_load(yml)['directory']
     storyboard_directories = glob.glob(f"{config}**/*.storyboard", recursive=True)
     dic1 = storyboard_directories[12]
-    # search_color(dic1)
+    search_color(dic1)
 
     # TODO: 一つで取得できたら、戻す 
-    for dic in storyboard_directories:
-        search_color(dic)
+    # for dic in storyboard_directories:
+    #     search_color(dic)
 
 def search_color(dic):
     print('===============================')
@@ -20,6 +20,8 @@ def search_color(dic):
     xml = open(dic)
     for line in xml.readlines():
         if 'red' in line:
-            print(line)
+            hoge = re.findall('red(.*)green', line)
+            red = re.findall('"(.*)"', hoge[0])[0]
+            print(red)
 
 main()
