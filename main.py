c47bef1f33d44c8be68461d9a2357117f3f46c76
tosh7 <zlia.6.lj.425@gmail.com>
2020-07-27 17:46:59 +0900

    export print method

--- main.py
+++ main.py
@@ -8,26 +8,28 @@ def main():
     with open('config/directory.yml') as yml:
         config = yaml.safe_load(yml)['directory']
     storyboard_directories = glob.glob(f"{config}**/*.storyboard", recursive=True)
-    dic1 = storyboard_directories[12]
-    # search_color(dic1)
+    xib_dictionaries = glob.glob(f"{config}**/*.xib", recursive=True)
 
-    # TODO: 一つで取得できたら、戻す 
     for dic in storyboard_directories:
         search_color(dic)
+    
+    for xib in xib_dictionaries:
+        search_color(xib)
 
 def search_color(dic):
-    print('===============================')
-    print(dic)
-    xml = open(dic)
-    for line in xml.readlines():
-        if 'red=' in line:
-            print(line)
-            red_line = re.findall('red(.*)green', line)
-            red = re.findall('"(.*)"', red_line[0])[0]
-            green_line = re.findall('green(.*)blue', line)
-            green = re.findall('"(.*)"', green_line[0])[0]
-            blue_line = re.findall('blue(.*)alpha', line)
-            blue = re.findall('"(.*)"', blue_line[0])[0]
-            print(rh.rgb_to_hex(float(red), float(green), float(blue)))
+    with open('file.txt', 'a') as f:
+        print('===============================', file=f)
+        print(dic, file=f)
+        xml = open(dic)
+        for line in xml.readlines():
+            if 'red=' in line:
+                print(line.strip(), file=f)
+                red_line = re.findall('red(.*)green', line)
+                red = re.findall('"(.*)"', red_line[0])[0]
+                green_line = re.findall('green(.*)blue', line)
+                green = re.findall('"(.*)"', green_line[0])[0]
+                blue_line = re.findall('blue(.*)alpha', line)
+                blue = re.findall('"(.*)"', blue_line[0])[0]
+                print(f'{rh.rgb_to_hex(float(red), float(green), float(blue))}\n', file=f)
 
 main()
