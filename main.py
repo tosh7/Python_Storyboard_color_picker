1e69bce91e11cb73ce8ad4a1e7fa6dca6038281e
tosh7 <zlia.6.lj.425@gmail.com>
2020-07-27 12:18:26 +0900

    got storyboards custom color info

--- main.py
+++ main.py
@@ -6,6 +6,20 @@ import yaml
 def main():
     with open('config/directory.yml') as yml:
         config = yaml.safe_load(yml)['directory']
-    print(glob.glob(f"{config}**/*.storyboard", recursive=True))
+    storyboard_directories = glob.glob(f"{config}**/*.storyboard", recursive=True)
+    dic1 = storyboard_directories[12]
+    # search_color(dic1)
+
+    # TODO: 一つで取得できたら、戻す 
+    for dic in storyboard_directories:
+        search_color(dic)
+
+def search_color(dic):
+    print('===============================')
+    print(dic)
+    xml = open(dic)
+    for line in xml.readlines():
+        if 'red' in line:
+            print(line)
 
 main()
